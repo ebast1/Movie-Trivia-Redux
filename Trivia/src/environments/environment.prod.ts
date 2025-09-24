df76cfe91a8dcfd8b8ced26c3e2182165d18c9db
ebast1 <ebast23@msu.idserve.net>
2025-09-24 17:09:23 -0400

    added s to http(s)

--- Trivia/src/environments/environment.prod.ts
+++ Trivia/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://your-production-api.com/api',
-  omdbApiUrl: 'http://www.omdbapi.com/',
+  omdbApiUrl: 'https://www.omdbapi.com/',
   omdbApiKey: '?apikey=8a8ceae&'
-
 };
